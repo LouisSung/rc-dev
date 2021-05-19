f61db8d2f8fde55a5f9b4dfed65d5e97bd7a1731
LouisSung <sjf.csie@gmail.com>
2021-05-19 20:42:12 +0800

    chore: rename images and Dockerfiles for build and runtime

--- docker/build.sh
+++ docker/build.sh
@@ -9,10 +9,10 @@ VER_METEOR='2.1.1'
 VER_APPS_ENGINE='1.25.0'
 
 THIS_SCRIPT='build.sh'
-DOCKERFILE_DEV='DockerfileDev'
-DOCKERFILE_ENV='DockerfileEnv'
 DOCKERFILE_BUILD='DockerfileBuild'
-Docker_ENTRYPOINT='docker-entrypoint.sh'
+DOCKERFILE_RUNTIME='DockerfileRuntime'
+DOCKERFILE_DEV='DockerfileDev'
+DOCKER_ENTRYPOINT='docker-entrypoint.sh'
 TMP_DOCKERFILE='.DockerfileTmp'
 TMP_METEOR_RELEASE='.MeteorRelease'
 TMP_PACKAGE_JSON='.PackageJson'
@@ -36,7 +36,7 @@ rm $TMP_DOCKERFILE $TMP_METEOR_RELEASE $TMP_PACKAGE_JSON
 
 # update versions
 echo "node@$VER_BASE, Rocket.Chat@$VER_ROCKET_CHAT, Meteor@$VER_METEOR, Apps Engine@$VER_APPS_ENGINE"
-for f in $DOCKERFILE_DEV $DOCKERFILE_BUILD; do
+for f in $DOCKERFILE_BUILD $DOCKERFILE_RUNTIME $DOCKERFILE_DEV; do
   sed -i "s/FROM node:.*/FROM node:$VER_BASE/" $f
   sed -i "s/\(install.meteor.com\/?release\)=[^ ]*/\1=$VER_METEOR/" $f
   sed -i "s/RC_VERSION=[^ ]*/RC_VERSION=$VER_ROCKET_CHAT/" $f
@@ -47,15 +47,15 @@ sed -i "s/\(FROM node:.*\)-slim/\1/" $DOCKERFILE_DEV  # use normal debian for de
 sed -i "0,/VER_BASE=.*/s/VER_BASE=.*/VER_BASE='$VER_BASE'/" $THIS_SCRIPT
 sed -i "0,/VER_METEOR=.*/s/VER_METEOR=.*/VER_METEOR='$VER_METEOR'/" $THIS_SCRIPT
 sed -i "0,/VER_APPS_ENGINE=.*/s/VER_APPS_ENGINE=.*/VER_APPS_ENGINE='$VER_APPS_ENGINE'/" $THIS_SCRIPT
-sed -i "s/louissung\/rc:dev-[^ ]*/louissung\/rc:dev-$VER_ROCKET_CHAT/" $Docker_ENTRYPOINT
-sed -i "s/louissung\/rc:build-[^ ]*/louissung\/rc:build-$VER_ROCKET_CHAT/" $Docker_ENTRYPOINT
+sed -i "s/louissung\/rc:runtime-[^ ]*/louissung\/rc:runtime-$VER_ROCKET_CHAT/" $DOCKER_ENTRYPOINT
+sed -i "s/louissung\/rc:dev-[^ ]*/louissung\/rc:dev-$VER_ROCKET_CHAT/" $DOCKER_ENTRYPOINT
 
 # build images
 docker build -t "louissung/rc:dev-$VER_ROCKET_CHAT" -f $DOCKERFILE_DEV .
 # comment out to copy bundle from the image
 # rm -rf ../app/bundle && mkdir -p ../app/bundle && docker run --name rc-bundle -v "$(pwd)/../app/bundle:/appBundle" "louissung/rc:dev-$VER_ROCKET_CHAT" bash -c 'cp -r /root/app/bundle/. /appBundle && chmod -R o+w /appBundle' && docker rm rc-bundle
-docker build -t "louissung/rc:env-$VER_ROCKET_CHAT" -f $DOCKERFILE_ENV .
 docker build -t "louissung/rc:build-$VER_ROCKET_CHAT" -f $DOCKERFILE_BUILD .
+docker build -t "louissung/rc:runtime-$VER_ROCKET_CHAT" -f $DOCKERFILE_RUNTIME .
 
 # >>>>>>> Script End
 cd "$DIR_RESTORE" || exit 1
